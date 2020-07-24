414fd5ce111f2c3d11696ebff6bbeb8eff19f5e4
Pablo Ingegnieri <pablo.ingegnieri@alumnos.ispi4038.edu.ar>
2020-07-24 12:54:38 -0300

    Agrego dtos faltantes e insercion de movimientos

--- modelo/finanzas.py
+++ modelo/finanzas.py
@@ -2,22 +2,50 @@ from dataclasses import dataclass
 import pymysql
 
 @dataclass
+class TransaccionDTO:
+    monto: float
+    categoria: object
+    descripcion: str
+    fecha: str
+    id_movimiento: int
+    id_categoria: int
+
+@dataclass
 class MovimientoDTO:
-    monto = float
-    categoria = object
-    descripcion = str
-    fecha = str
+    nombre: str
+    descripcion: str
 
 @dataclass
 class CategoriaDTO:
-    nombre = str
-    descripcion = str
+    nombre: str
+    descripcion: str
 
-class Database:
-    def __init__(self):
-        self.conexion = pymysql.connect(#database)
-        self.cursor = self.db.cursor()
-    
+
 class Service:
     def __init__(self):
-        self.database = Database()
+        self.conexion = pymysql.connect(host="localhost", port=3306, user="usuario",
+                                        passwd="1234", db="finanzas")
+        self.cursor = self.conexion.cursor()
+
+    def registrar_movimiento(self, data = MovimientoDTO):
+        self.cursor.execute(
+            "INSERT INTO movimientos VALUES (%s, %s, %s)", (None, data.nombre, data.descripcion)
+        )
+        self.conexion.commit()
+        pass
+
+    def registrar_categoria(self, data = CategoriaDTO):
+        pass
+
+    def calcular_valance(self):
+        pass
+
+    def cerrar_database(self):
+        self.conexion.close()
+
+if __name__ == "__main__":
+    movimiento = MovimientoDTO('juan', 'hola')
+    modelo = Service()
+    
+    modelo.registrar_movimiento(movimiento)
+    modelo.cerrar_database()
