5699bcc42db0f4f25827b4888bee3fc7c971f446
Juan Pablo <jpablo.pellegrini97@gmail.com>
2020-07-27 12:35:49 -0300

    agrego label de balance y botones calendario, + y -

--- vista/finanzas.py
+++ vista/finanzas.py
@@ -10,6 +10,18 @@ class Vista(QtWidgets.QWidget):
     def __setupUi(self):
         self.__layout = QtWidgets.QFormLayout()
 
+        self.__label_balance = QtWidgets.QLabel('$0')
+        self.__btn_calendario = QtWidgets.QPushButton("Calendario")
+        self.__btn_ingreso = QtWidgets.QPushButton("+")
+        self.__btn_gasto = QtWidgets.QPushButton("-")
+        
+        self.__layout.addRow("Balance: ", self.__label_balance)
+        self.__layout.addRow(self.__btn_calendario)
+        self.__layout.addRow(self.__btn_ingreso)
+        self.__layout.addRow(self.__btn_gasto)
+        
+        self.setLayout(self.__layout)
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     vista = Vista()
