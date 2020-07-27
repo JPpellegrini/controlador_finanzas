3b23812036a0ed78701df878c631b3f1114a3c68
Pablo Ingegnieri <pablo.ingegnieri@alumnos.ispi4038.edu.ar>
2020-07-27 12:03:45 -0300

    agrego metodos 'editar', 'eliminar', 'buscar', los metdos 'registrar' faltantes y 'calcular_valance'

--- modelo/finanzas.py
+++ modelo/finanzas.py
@@ -1,14 +1,14 @@
 from dataclasses import dataclass
 import pymysql
 
+
 @dataclass
 class TransaccionDTO:
     monto: float
-    categoria: object
-    descripcion: str
-    fecha: str
     id_movimiento: int
     id_categoria: int
+    descripcion: str
+    fecha: str
 
 @dataclass
 class MovimientoDTO:
@@ -27,6 +27,29 @@ class Service:
                                         passwd="1234", db="finanzas")
         self.cursor = self.conexion.cursor()
 
+    def buscar(self, tabla, campo = None, condicion = None, valor = None):
+        if campo:
+            self.cursor.execute(
+                "SELECT * FROM {} WHERE {} {} %s".format(tabla, campo, condicion), (valor)
+            )
+        else:
+            self.cursor.execute(
+                "SELECT * FROM {}".format(tabla)
+            )
+        return self.cursor.fetchall()
+
+    def eliminar(self, tabla, id):
+        self.cursor.execute(
+            "DELETE FROM {} WHERE id = %s".format(tabla), (id)
+        )
+        self.conexion.commit()
+
+    def editar(self, tabla, campo, valor):
+        self.cursor.execute(
+            "UPDATE {} SET {} = %s".format(tabla, campo), (valor)
+        )
+        self.conexion.commit()
+
     def registrar_movimiento(self, data = MovimientoDTO):
         self.cursor.execute(
             "INSERT INTO movimientos VALUES (%s, %s, %s)", (None, data.nombre, data.descripcion)
@@ -35,19 +58,31 @@ class Service:
         
     def registrar_categoria(self, tipo, data = CategoriaDTO):
         self.cursor.execute(
-            "INSERT INTO categorias_{} VALUES (%s, %s, %s)".format(tipo), (None, data.nombre, data.descripcion)
+            "INSERT INTO {} VALUES (%s, %s, %s)".format(tipo), (None, data.nombre, data.descripcion)
+        )
+        self.conexion.commit()
+
+    def registrar_transaccion(self, tipo, data = TransaccionDTO):
+        self.cursor.execute(
+            "INSERT INTO {} VALUES (%s, %s, %s, %s, %s, %s)".format(tipo), (None, data.monto, data.id_movimiento,
+                                                                            data.id_categoria, data.descripcion, data.fecha,)
         )
         self.conexion.commit()
 
     def calcular_valance(self):
-        pass
+        valance = 0
+        for ingreso in self.buscar("ingresos"):
+            valance += ingreso[1]
+        for egreso in self.buscar("egresos"):
+            valance -= egreso[1]
+        return valance
 
     def cerrar_database(self):
         self.conexion.close()
 
 if __name__ == "__main__":
-    categoria = CategoriaDTO('juan', 'hola')
+    transaccion = TransaccionDTO(0.50, 1, 1, "", "2019")
     modelo = Service()
     
-    modelo.registrar_categoria("egreso", categoria)
+    print(modelo.calcular_valance())
     modelo.cerrar_database()
