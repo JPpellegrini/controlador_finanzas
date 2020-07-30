c220939d7261f652d8c12396aa566b95b8fefe48
Juan Pablo Pellegrini <jpablo.pellegrini@gmail.com>
2020-07-30 16:25:28 -0300

    Agrego ventana de nuevo movimiento

--- vista/finanzas.py
+++ vista/finanzas.py
@@ -1,6 +1,28 @@
 import sys
 from PyQt5 import QtCore, QtWidgets, QtGui
 
+class Ventana_movimiento(QtWidgets.QDialog):
+    def __init__(self, parent = None):
+        QtWidgets.QDialog.__init__(self, parent)
+        self.__setupUi()
+    
+    def __setupUi(self):
+        self.__contenedor = QtWidgets.QVBoxLayout()
+
+        #WIDGETS
+        self.__line_nombre = QtWidgets.QLineEdit()
+        self.__line_descripcion = QtWidgets.QTextEdit()
+        self.__boton1 = QtWidgets.QPushButton("Aceptar")
+        
+        self.__line_nombre.setPlaceholderText("Nombre")
+        self.__line_descripcion.setPlaceholderText("Descripción")
+        
+        self.__contenedor.addWidget(self.__line_nombre)
+        self.__contenedor.addWidget(self.__line_descripcion)
+        self.__contenedor.addWidget(self.__boton1)
+
+        self.setLayout(self.__contenedor)
+
 class Vista(QtWidgets.QWidget):
     
     calcular_balance = QtCore.pyqtSignal()
@@ -20,15 +42,18 @@ class Vista(QtWidgets.QWidget):
         self.__line_cantidad = QtWidgets.QLineEdit()
         self.__btn_ingreso = QtWidgets.QPushButton("+")
         self.__btn_egreso = QtWidgets.QPushButton("-")
+        self.__btn_movimiento = QtWidgets.QPushButton("Agregar movimiento")
         
         self.__layout.addRow("Balance: ", self.__label_balance)
         self.__layout.addRow(self.__btn_calendario)
         self.__layout.addRow(self.__line_cantidad)
         self.__layout.addRow(self.__btn_ingreso)
         self.__layout.addRow(self.__btn_egreso)
+        self.__layout.addRow(self.__btn_movimiento)
 
         self.__btn_ingreso.clicked.connect(self.__on_btn_ingreso_clicked)
         self.__btn_egreso.clicked.connect(self.__on_btn_egreso_clicked)
+        self.__btn_movimiento.clicked.connect(self.__on_btn_movimiento_clicked)
                 
         self.setLayout(self.__layout)
     
@@ -40,6 +65,9 @@ class Vista(QtWidgets.QWidget):
         self.agregar_egreso.emit()
         self.calcular_balance.emit()
 
+    def __on_btn_movimiento_clicked(self):
+        self.__ventana_agragar_movimiento = Ventana_movimiento().exec_()
+    
     def obtener_datos(self):
         return float(self.__line_cantidad.text())
 
