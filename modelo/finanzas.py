2c79480a6a207eae102a2fe547710ad469b55e39
Pablo Ingegnieri <pablo.ingegnieri@alumnos.ispi4038.edu.ar>
2020-08-14 12:06:18 -0300

    Agrego service de tipo de transaccion

--- modelo/finanzas.py
+++ modelo/finanzas.py
@@ -22,9 +22,52 @@ class CategoriaDTO:
 
 class Database:
     def __init__(self):
-        self.conexion = pymysql.connect(host="localhost", port=3306, user="usuario",
+        self.__conexion = pymysql.connect(host="localhost", port=3306, user="usuario",
                                         passwd="1234", db="finanzas")
-        self.cursor = self.conexion.cursor()
+        self.__cursor = self.__conexion.cursor()
+    
+    def ejecutar(self, sentencia, argumentos = None):
+        self.__cursor.execute(sentencia, argumentos)
+        return self.__cursor
+    
+    def guardar(self):
+        self.__conexion.commit()
     
     def cerrar_database(self):
-        self.conexion.close()
+        self.__conexion.close()
+
+class Service_tipo_transaccion():
+    def __init__(self, database):
+        self.database = database
+
+    def registrar(self, data = Tipo_transaccionDTO):
+        if data.nombre != "":
+            self.database.ejecutar(
+                "INSERT INTO movimientos VALUES (%s, %s, %s)", (None, data.nombre, data.descripcion)
+            )
+            self.database.guardar()
+        else : return "Ingrese el nombre"
+    
+    def editar(self, id, data = Tipo_transaccionDTO):
+        if data.nombre != "":
+            self.database.ejecutar(
+                "UPDATE movimientos SET nombre=%s, descripcion=%s WHERE id = %s", (data.nombre, data.descripcion, id)
+            )
+            self.database.guardar()
+        else : return "Ingrese el nombre"
+
+    def eliminar(self, id):
+        self.database.ejecutar(
+            "DELETE FROM movimientos WHERE id = %s", (id)
+        )
+        self.database.guardar()
+    
+    def obtener_tipos(self):
+        return self.database.ejecutar(
+            "SELECT * FROM movimientos"
+        ).fetchall()
+    
+if __name__ == "__main__":
+    database = Database()
+    service = Service_tipo_transaccion(database)
+    tipo1 = Tipo_transaccionDTO("prueba", "comentario")
