00ea9532dd5a4b720e7e9e62acf10899a8a224e0
Pablo Ingegnieri <pablo.ingegnieri@alumnos.ispi4038.edu.ar>
2020-09-01 10:42:59 -0300

    Creo diccionario con variables de entorno

--- main.py
+++ main.py
@@ -6,10 +6,15 @@ from modelo.modelo import Database
 from dotenv import load_dotenv
 load_dotenv()
 
-database = Database.get(os.getenv("MYSQL_USERNAME"), os.getenv("MYSQL_PASSWORD"))
-app = QtWidgets.QApplication(sys.argv)
-controlador = Controlador()
 
+db_user = dict(
+    username = os.getenv("MYSQL_USERNAME"),
+    password = os.getenv("MYSQL_PASSWORD"),
+)
+database = Database.get(db_user["username"],db_user["password"])
+
+app = QtWidgets.QApplication(sys.argv)
 app.aboutToQuit.connect(database.close)
+controlador = Controlador()
 controlador.show_vista()
 app.exec()
