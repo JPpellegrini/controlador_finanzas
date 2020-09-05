ec3ef41a029757c9a50318ce0d11417603b2b678
Pablo Ingegnieri <pablo.ingegnieri@alumnos.ispi4038.edu.ar>
2020-09-05 01:03:19 -0300

    Creo archivo ingreso.py con Service de ingreso

---
+++ modelo/ingreso.py
@@ -0,0 +1,77 @@
+from dataclasses import dataclass
+from recursos import Database
+from tipo_transaccion import ServiceTipoTransaccion
+from categoria_ingreso import ServiceCategoriaIngreso
+
+
+@dataclass
+class IngresoDTO:
+    monto: str
+    id_tipo_transaccion: int
+    id_categoria: int
+    descripcion: str
+    fecha: str
+    id: int=None
+    
+class MontoError(Exception):
+    def __str__(self):
+        return "Monto invalido"
+
+class TipoError(Exception):
+    def __str__(self):
+        return "Tipo invalido"
+
+class CategoriaError(Exception):
+    def __str__(self):
+        return "Categoria invalida"
+
+class ServiceIngreso:
+    def __init__(self):
+        self.database = Database.get()
+        self.cursor = self.database.cursor()
+        self.srv_tipos = ServiceTipoTransaccion()
+        self.srv_categorias = ServiceCategoriaIngreso()
+
+    def registrar_ingreso(self, data: IngresoDTO):
+        if not data.id_tipo_transaccion:
+            raise TipoError
+        if not data.id_categoria:
+            raise CategoriaError
+        try:
+            self.cursor.execute(
+                "INSERT INTO ingresos (id, monto, tipo, categoria_ingreso, descripcion, fecha) VALUES (%s, %s, %s, %s, %s, %s)",\
+                (data.id, data.monto, data.id_tipo_transaccion, data.id_categoria, data.descripcion, data.fecha)
+            )
+            self.database.commit()
+        except pymysql.Error:
+            raise MontoError
+    
+    def editar_ingreso(self, data: IngresoDTO):
+        if not data.id_tipo_transaccion:
+            raise TipoError
+        if not data.id_categoria:
+            raise CategoriaError
+        try:
+            self.cursor.execute(
+                "UPDATE ingresos SET monto=%s, tipo=%s, categoria_ingreso=%s, descripcion=%s, fecha=%s WHERE id = %s",\
+                (data.monto, data.id_tipo_transaccion, data.id_categoria, data.descripcion, data.fecha, data.id)
+            )
+            self.database.commit()
+        except pymysql.Error:
+            raise MontoError
+
+    def eliminar_ingreso(self, data: IngresoDTO):
+        self.cursor.execute(
+            "DELETE FROM ingresos WHERE id = %s", data.id
+        )
+        self.database.commit()
+    
+    def obtener_ingresos(self):
+        self.cursor.execute(
+            "SELECT i.id, i.monto, t.nombre as tipo, c.nombre as categoria, i.descripcion, i.fecha FROM ingresos i JOIN\
+             tipos_transaccion t ON i.tipo=t.id JOIN categorias_ingreso c ON i.categoria_ingreso=c.id"
+        )
+        return [IngresoDTO(ingreso["monto"], ingreso["tipo"], ingreso["categoria"], ingreso["descripcion"], ingreso["fecha"], ingreso["id"]) for ingreso in self.cursor.fetchall()]
+    
+    def obtener_tipos_categorias(self):
+        return dict(tipos = self.srv_tipos.obtener_tipos(), categorias = self.srv_categorias.obtener_cat_ingreso())
