c448daf0ad4e20457d0209304b05d49226a976e3
Pablo Ingegnieri <pablo.ingegnieri@alumnos.ispi4038.edu.ar>
2020-09-28 20:15:15 -0300

    Creo clase Session en recursos.py para trabajar con sqlalchemy

--- modelo/recursos.py
+++ modelo/recursos.py
@@ -1,4 +1,20 @@
+import os
+
 import pymysql
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+username = os.getenv("MYSQL_USERNAME")
+password = os.getenv("MYSQL_PASSWORD")
+
+engine = create_engine(f"mysql+pymysql://{username}:{password}@localhost/finanzas")
+
+Session = sessionmaker(bind=engine)
+
+Base = declarative_base()
 
 
 class Database:
